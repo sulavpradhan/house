080639a97387b009eb6cd8590d1917fd7a453e17
sulavpradhan <sulavpradhan35@gmail.com>
2022-01-30 08:55:07 +0545

    navbar component added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
-
+import Explore from "./pages/Explore";
+import Profile from "./pages/Profile";
+import Offers from "./pages/Offers";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import ForgotPassword from "./pages/ForgotPassword";
+import NavBar from "./components/NavBar";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <>
-    <h1>home page</h1>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Explore />} />
+          <Route path="/profile" element={<SignIn />} />
+          <Route path="/offers" element={<Offers />} />
+          <Route path="/sign-in" element={<SignIn />} />
+          <Route path="/sign-up" element={<SignUp />} />
+          <Route path="/forgot-password" element={<ForgotPassword />} />
+        </Routes>
+        <NavBar />
+      </Router>
     </>
   );
 }
